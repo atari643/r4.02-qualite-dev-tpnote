1328eaefa7025a0d6cb2792ed649b7c0fc8bd004
Paul Skutecki <paul.skutecki@etu.u-bordeaux.fr>
2024-03-15 11:16:36 +0100

    ajout header test

--- .gitlab/test_gitignore.sh
+++ .gitlab/test_gitignore.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/env sh
+###
+#
+#  @file test_gitignore.sh
+#  @copyright 2023-2024 Univ. Bordeaux. All rights reserved.
+#
+#  @version 1.2.3
+#  @author SKUTECKI Paul
+#  @date 2024-03-09
+#
+# This script check that .gitignore exist
+#
+###
+
 if [ -f ../.gitignore ]; then
     echo 'File exists.'
     exit 0
